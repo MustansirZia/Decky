05bba48a13d9b383b0e400c896f1ef5dce66eda5
Mustansir Zia <mustansir29@hotmail.ckm>
2017-04-03 13:10:24 +0530

    imports optimized

--- app/src/main/java/com/mz/cards/MainActivity.java
+++ app/src/main/java/com/mz/cards/MainActivity.java
@@ -1,21 +1,16 @@
 package com.mz.cards;
 
 import android.app.ProgressDialog;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.mutualmobile.cardstack.CardStackLayout;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
